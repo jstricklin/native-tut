efb203f49874a76060fa9844493ab2f81942ad26
Jose Stricklin <josestricklin@El-Diablo.local>
2018-10-29 13:17:32 -0600

    working map

--- FirstNativeProject/App.js
+++ FirstNativeProject/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, TextInput, View, Button, Dimensions } from 'react-native';
 import FetchLocation from './components/FetchLocation';
 // import MapView from './components/map/MapView.js'
 import MapView from 'react-native-maps'
@@ -19,12 +19,38 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+var { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const baseURL = 'http://www.mapquestapi.com/geocoding/v1/address?key=AvgVnMA8ethCWCJijX8T0aDMz0YWzUi9&location='
+
 type Props = {};
 export default class App extends Component<Props> {
+    constructor(props){
+        super(props)
+        this.state = {
+            text: '',
+            locationCoords: {
+                lat: "",
+                lng: "",
+            }
+        }
+    }
     getUserLocationHandler = () => {
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position)
+            // this.setState({ locationCoords: {position.coords }})
+            console.log(position.coords)
+            console.log(ASPECT_RATIO)
         }, err => console.log(err))
+
+    }
+    textChangeHandler = (text) => {
+        this.setState({ text: text })
+    }
+    updateMapLocation = () => {
+        let searchAddy = fetch(`${baseURL}${this.state.text}`)
+            .then(res => res.json())
+            .then(json => this.setState({locationCoords: json.results[0].locations[0].displayLatLng}))
 
     }
     render() {
@@ -32,8 +58,18 @@ export default class App extends Component<Props> {
             <View style={styles.container}>
                 <Text style={styles.welcome}>Welcome to Jose's First Native App!</Text>
                 <FetchLocation onGetLocation={this.getUserLocationHandler}/>
+                <TextInput style={{ height: 50, backgroundColor: 'white', width: 250 }} placeholder="Search for a Location Now!" onChangeText={this.textChangeHandler} />
+                <Text>{this.state.text} {this.state.locationCoords.lat} {this.state.locationCoords.lng}</Text>
+                <Button title="search" onPress={this.updateMapLocation} />
                 <View style={{ height: 200, width: 200 }}>
-                    <MapView style={styles.map} />
+                    <MapView style={styles.map}
+                        region={{
+                        latitude: this.state.locationCoords.lat,
+                        longitude: this.state.locationCoords.lng,
+                        latitudeDelta: .01,
+                        longitudeDelta: .01 * ASPECT_RATIO,
+                        }}
+                    />
                 </View>
             </View>
             );
@@ -45,7 +81,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        // backgroundColor: 'skyblue',
+        backgroundColor: 'skyblue',
     },
     map: {
         ...StyleSheet.absoluteFillObject,
